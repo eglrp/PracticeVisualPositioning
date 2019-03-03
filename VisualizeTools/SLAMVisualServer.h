45d6457106b9044b682063314847da438456545d
steve <551619855@qq.com>
2019-03-03 14:17:47 +0800

    ...

--- VisualizeTools/SLAMVisualServer.h
+++ VisualizeTools/SLAMVisualServer.h
@@ -5,10 +5,42 @@
 #ifndef BASESLAM_SLAMVISUALSERVER_H
 #define BASESLAM_SLAMVISUALSERVER_H
 
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
 #include <opencv2/viz.hpp>
 
 namespace BaseSLAM {
 	class SLAMVisualServer {
+	public:
+		std::string windows_name_ = "default";
+
+		cv::viz::Viz3d windows_;//(windows_name_);
+		cv::viz::WTrajectory* wTrajectory_ptr_=nullptr;//default trajectory.
+
+		std::vector<cv::Affine3d> pose_vec_;
+
+		SLAMVisualServer(std::string windows_name) {
+			windows_name_ = windows_name;
+			windows_ = cv::viz::Viz3d(windows_name_);
+//			wTrajectory_ = cv::viz::WTrajectory(pose_vec_);
+//			windows_.showWidget("",)
+		}
+
+
+		bool addNewPose(cv::Affine3d pose){
+			pose_vec_.push_back(pose);
+			if(wTrajectory_ptr_==nullptr){
+				wTrajectory_ptr_ = new cv::viz::WTrajectory(pose_vec_);
+				windows_.showWidget("trajectory",*wTrajectory_ptr_);
+			}else{
+				wTrajectory_ptr_->updatePose(pose);
+			}
+			return true;
+		}
+
+
+
 
 	};
 
--- tester/slam_visual_server_tes.cpp
+++ tester/slam_visual_server_tes.cpp
@@ -9,8 +9,24 @@
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
+
+#include "VisualizeTools/SLAMVisualServer.h"
+#include "VisualizeTools/SLAMVisualServer.cpp"
+
 int main()
 {
+
+	auto visual_server = BaseSLAM::SLAMVisualServer("test");
+
+	for(int k=0;k<1000;++k){
+		cv::Affine3d fake_pose;
+		fake_pose.matrix(0,3)=double(k)/500.0;
+		visual_server.addNewPose(fake_pose);
+		cv::waitKey(1);
+	}
+
+	cv::waitKey(0);
+
 
 
 

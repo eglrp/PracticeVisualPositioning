0815794fff3fa1838437696b0ea314bb8f4c87b1
steve <551619855@qq.com>
2019-04-11 09:52:17 +0800

    stere config server.

---
+++ StereoVO/MarginalizationServer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by steve on 4/11/19.
+//
+
+#include "MarginalizationServer.h"
---
+++ StereoVO/MarginalizationServer.h
@@ -0,0 +1,18 @@
+//
+// Created by steve on 4/11/19.
+//
+
+#ifndef PRACTICEVISUALPOSITIONING_MARGINALIZATIONSERVER_H
+#define PRACTICEVISUALPOSITIONING_MARGINALIZATIONSERVER_H
+
+#include <ceres/ceres.h>
+
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+class MarginalizationServer {
+
+};
+
+
+#endif //PRACTICEVISUALPOSITIONING_MARGINALIZATIONSERVER_H
